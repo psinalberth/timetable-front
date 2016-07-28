669fb05882228fb7106240bd1cde9bd6390f1736
Inalberth Pinheiro <inalberth07@gmail.com>
2016-07-27 22:02:35 -0300

    Criando composer para Professor.

---
+++ timetable-front/src/main/java/br/edu/ifma/csp/timetable/composer/ProfessorComposer.java
@@ -0,0 +1,19 @@
+package br.edu.ifma.csp.timetable.composer;
+
+import br.edu.ifma.csp.timetable.model.Professor;
+
+public class ProfessorComposer extends Composer<Professor> {
+
+	private static final long serialVersionUID = -2980484980640308048L;
+
+	@Override
+	public void init() {
+		
+	}
+	
+	@Override
+	public void save() {
+
+		super.save();
+	}
+}
