1c7bee31901f19c66642e6141e1fc93d79715124
Inalberth Pinheiro <inalberth07@gmail.com>
2016-08-05 06:33:40 -0300

    Refatorando projeto:

--- timetable-front/src/main/java/br/edu/ifma/csp/timetable/composer/TurmaComposer.java
+++ timetable-front/src/main/java/br/edu/ifma/csp/timetable/composer/TurmaComposer.java
@@ -1,5 +1,8 @@
 package br.edu.ifma.csp.timetable.composer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.zkoss.bind.annotation.Init;
 
 import br.edu.ifma.csp.timetable.model.Turma;
@@ -7,11 +10,26 @@ import br.edu.ifma.csp.timetable.model.Turma;
 public class TurmaComposer extends Composer<Turma>{
 
 	private static final long serialVersionUID = 7381784160763808819L;
+	
+	private List<Integer> colCodigos;
 
 	@Init
 	@Override
 	public void init() {
 		
+		colCodigos = new ArrayList<Integer>();
+		colCodigos.add(1);
+		colCodigos.add(2);
+		colCodigos.add(3);
+		colCodigos.add(4);
+		colCodigos.add(20);
 	}
-
+	
+	public List<Integer> getColCodigos() {
+		return colCodigos;
+	}
+	
+	public void setColCodigos(List<Integer> colCodigos) {
+		this.colCodigos = colCodigos;
+	}
 }
--- timetable-front/src/main/java/br/edu/ifma/csp/timetable/model/Curso_.java
+++ timetable-front/src/main/java/br/edu/ifma/csp/timetable/model/Curso_.java
@@ -5,13 +5,13 @@ import javax.persistence.metamodel.SetAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2016-08-03T12:44:33.763-0300")
+@Generated(value="Dali", date="2016-08-04T18:31:50.706-0300")
 @StaticMetamodel(Curso.class)
 public class Curso_ extends Entidade_ {
 	public static volatile SingularAttribute<Curso, Integer> id;
-	public static volatile SingularAttribute<Curso, String> codigo;
 	public static volatile SingularAttribute<Curso, String> nome;
 	public static volatile SingularAttribute<Curso, String> descricao;
 	public static volatile SetAttribute<Curso, Turma> periodos;
 	public static volatile SetAttribute<Curso, MatrizCurricular> matrizes;
+	public static volatile SingularAttribute<Curso, String> codigo;
 }
