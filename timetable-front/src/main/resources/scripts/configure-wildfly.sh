59c47b9f7a76522d512dd3cb81cd935a571461e9
psinalberth <inalberth07@gmail.com>
2016-10-17 14:16:55 -0300

    Refatorando projeto:
     - Remoção de arquivos não utilizados;
     - Organização de arquivos do projeto;

--- timetable-front/src/main/resources/scripts/configure-wildfly.sh
+++ timetable-front/src/main/resources/scripts/configure-wildfly.sh
@@ -1,4 +1,4 @@
 mkdir -p target/wildfly-8.2.1.Final/modules/system/layers/base/com/mysql/main
-cp src/main/resources/standalone.xml target/wildfly-8.2.1.Final/standalone/configuration/
-cp src/main/resources/module.xml target/wildfly-8.2.1.Final/modules/system/layers/base/com/mysql/main/
-cp src/main/resources/mysql-connector-java-5.1.39.jar target/wildfly-8.2.1.Final/modules/system/layers/base/com/mysql/main/
+cp src/main/resources/configuration/standalone.xml target/wildfly-8.2.1.Final/standalone/configuration/
+cp src/main/resources/configuration/module.xml target/wildfly-8.2.1.Final/modules/system/layers/base/com/mysql/main/
+cp src/main/resources/drivers/mysql-connector-java-5.1.39.jar target/wildfly-8.2.1.Final/modules/system/layers/base/com/mysql/main/
